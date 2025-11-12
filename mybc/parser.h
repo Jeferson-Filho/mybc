20f8d217fab42e194ba058f716cc1076e30a332e
Jeferson-Filho <jpdietrichfilho.2015@gmail.com>
2025-11-12 08:35:02 -0300

    fix: ajuste na declaracao de handleSigint

--- mybc/parser.h
+++ mybc/parser.h
@@ -16,7 +16,7 @@ extern void match(int);
 
 extern int gettoken(FILE *);
 
-extern void handleSigint(void);
+extern void handleSigint(int sig);
 extern void mybc(void);
 extern void cmd(void);
 extern void E(void);
